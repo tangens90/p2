fa134ef17e2e76113f1036746bd1b0c28b523cc9
Jakub Parapura <jakubparapura@gmail.com>
2021-03-05 12:49:15 +0100

    added zadanie1_c_style.cpp and example input file

---
+++ homework/zestaw1/zadanie1_c_style.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	const char** nazwiska = new const char*[n];
+
+	for (int i = 0; i < n; i++) {
+		char* tmp;
+		// %ms powoduje sparsowanie napisu i alokacje takiej
+		// ilości pamięci, żeby zmieścić podany napis, znak
+		// \0 i nic więcej.
+		scanf("%ms", &tmp);
+		nazwiska[i] = tmp;
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << nazwiska[i] << " " << strlen(nazwiska[i]) << endl;
+	}
+
+
+	for (int i = 0; i < n; i++) {
+		delete[] nazwiska[i];
+	}
+
+	delete[] nazwiska;
+}
