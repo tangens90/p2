4d4993f78dad635f8117f266702b4281843ff676
Jakub Parapura <jakubparapura@gmail.com>
2021-03-28 09:38:46 +0200

    działa Emplace, Insert i Print

---
+++ p2/BaCa/operacje_zbiorowe/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "source.cpp"
+#include <bitset>
+
+using namespace std;
+
+int main() {
+	int test1 = 0b01101;
+	//string e1 = "   00110         ";
+	char e1[] = "   00110         ";
+	Emplace(&e1[0], &test1);
+	bitset<32> bity1 = test1;
+	cout << bity1 << endl;
+
+	int test2 = 0b00000;
+	char e2[] = " 00011   00010     11111   00110    11110 00100 00001    00000";
+	Insert(&e2[0], &test2);
+	bitset<32> bity2 = test2;
+	cout << bity2 << endl;
+
+	char e3[1000];
+	Print(test2, &e3[0]);
+	cout << e3 << endl;
+
+
+	int test3 = 0b00010;
+	cout << boolalpha << Emptiness(test3) << endl;
+	bitset<32> bity3 = test3;
+	cout << bity3 << endl;
+
+	int test4 = 0b011;
+	cout << boolalpha << Emptiness(test4) << endl;
+	bitset<32> bity4 = test4;
+	cout << bity4 << endl;
+	char e4[1000];
+	Print(test4, &e4[0]);
+	cout << e4 << endl;
+
+	int test5 = -1;
+	bitset<32> bity5 = test5;
+	cout << bity5 << endl;
+	char e5[1000];
+	Print(test5, &e5[0]);
+	cout << e5 << endl;
+}
---
+++ p2/BaCa/operacje_zbiorowe/source.cpp
@@ -0,0 +1,97 @@
+// Jakub Parapura
+
+void InsertRec(char* input, int* set, int i, int power, int sum, bool reading) {
+	i++;
+	if (*(input + i) == ' ' || *(input + i) == '\0') {
+		// nic lub znaleziono ciąg
+		
+		// wstawiamy znalezioną sume
+		if (reading) {
+			*set = *set | 1 << sum;
+		}
+		// resetujemy wartości
+		if (*(input + i) != '\0') {
+			InsertRec(input, set, i, 32, 0, false);
+		}
+	}
+	else if (*(input + i) == '0') {
+		reading = true;
+		power /= 2; // TODO zamienić na przesunięćie bitowe
+		InsertRec(input, set, i, power, sum, true);
+
+	}
+	else if (*(input + i) == '1') {
+		reading = true;
+		power /= 2; // TODO zamienić na przesunięćie bitowe
+		sum += power;
+		InsertRec(input, set, i, power, sum, true);
+	}
+
+}
+
+void Emplace(char* input, int* set) {
+	*set = 0;
+	InsertRec(input, set, -1, 32, 0, false);
+}
+
+void Insert(char* input, int* set) {
+	InsertRec(input, set, -1, 32, 0, false);
+}
+
+bool Emptiness(int set) {
+	return set == 0;
+}
+
+void calcSetElement(int power, int toPrint, char* output, int output_idx) {
+	if (power == 0)
+		return;
+
+	if (toPrint >= power) {
+		*(output + output_idx) = '1';
+		toPrint -= power;
+	}
+	else {
+		*(output + output_idx) = '0';
+	}
+	power /= 2; // TODO przesunięcie bitowe
+	output_idx++;
+	calcSetElement(power, toPrint, output, output_idx);
+}
+
+int PrintRec(int set, char* output, int i, int output_idx) {
+	i--;
+	if (i == -1) 
+		return output_idx;
+
+	int tmp = set;
+	tmp = tmp >> i;
+	if (set & (1 << i)) {
+		int power = 16;
+		int toPrint = i;
+		calcSetElement(power, toPrint, output, output_idx);
+		output_idx += 5;
+		*(output + output_idx) = ' ';
+		output_idx++;
+		return PrintRec(set, output, i, output_idx);
+	}
+	else {
+		// nic
+		return PrintRec(set, output, i, output_idx);
+	} 
+}
+
+void Print(int set, char* output) {
+	if (Emptiness(set)) {
+		*(output + 0) = 'e';
+		*(output + 1) = 'm';
+		*(output + 2) = 'p';
+		*(output + 3) = 't';
+		*(output + 4) = 'y';
+		*(output + 5) = '\0';
+		return;
+	}
+
+	// TODO bez spacji na końcu i z \0
+	int output_idx = PrintRec(set, output, 32, 0);
+	*(output + output_idx - 1) = '\0';
+}
