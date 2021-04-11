aba5297e49007bf060a5989ffb61384ba907a152
Jakub Parapura <jakubparapura@gmail.com>
2021-04-11 23:19:18 +0200

    zielony pasek

--- mn/pochodne_mieszane/funkcja.h
+++ mn/pochodne_mieszane/funkcja.h
@@ -1,10 +1,13 @@
 template <typename T>
 T funkcja(const T& x, const T& y) {
-	//return x+y;
+	T wynik = sin(x*x - 2*(y+1))/exp(-y*y+cos(x*y));
+	//T wynik = sin(x+y);
+	//T wynik = sin((x-y)/(3*x+5));
+	//T wynik = ((x+y)*(y-3))/(-y-x*y-2);
 	//T wynik = x+y;
 	//T wynik = x+x*y+y;
 	//T wynik = 3.0+x+y+5.0;
 	//T wynik = 3.0+x+y+5.0;
-	T wynik = x/y;
+	//T wynik = (2 + x)/y;
 	return wynik;
-}
+
--- mn/pochodne_mieszane/source.cpp
+++ mn/pochodne_mieszane/source.cpp
@@ -1,4 +1,6 @@
+// Jakub Parapura
 #include <iostream>
+#include <iomanip>
 #include <math.h>
 #include "funkcja.h"
 
@@ -19,7 +21,32 @@ public:
 
 	FAD(double val, double dx, double dy, double dxdx, double dxdy, double dydy) : val(val), dx(dx), dy(dy), dxdx(dxdx), dxdy(dxdy), dydy(dydy) {}
 
-	FAD() {}
+	void operator=(const FAD& fad) {
+		val = fad.val;
+		dx = fad.dx;
+		dy = fad.dy;
+		dxdx = fad.dxdx;
+		dxdy = fad.dxdy;
+		dydy = fad.dydy;
+	}
+
+	FAD() {
+		val   = 0;
+		dx    = 0;
+		dy    = 0;
+		dxdx  = 0;
+		dxdy  = 0;
+		dydy  = 0;
+	}
+
+	FAD(const FAD& fad) {
+		val = fad.val;
+		dx = fad.dx;
+		dy = fad.dy;
+		dxdx = fad.dxdx;
+		dxdy = fad.dxdy;
+		dydy = fad.dydy;
+	}
 
 	friend ostream& operator<<(ostream& out, const FAD& fad) {
 		out << fad.val << " " << fad.dx << " " << fad.dy << " " << fad.dxdx << " " << fad.dxdy << " " << fad.dydy << endl;
@@ -110,23 +137,61 @@ FAD operator*(const FAD& lhs, double value) {
 
 // value / FAD
 FAD operator/(double value, const FAD& rhs) {
-	FAD ret(rhs.val / value, -value * rhs.dx / (rhs.val * rhs.val), -value * rhs.dy / (rhs.val * rhs.val),
-		(value * (2 * (rhs.dx * rhs.dx) - rhs.val * rhs.dxdx)) / (rhs.val * rhs.val * rhs.val), 
-		(value * (2 * (rhs.dx * rhs.dy) - rhs.val * rhs.dxdy)) / (rhs.val * rhs.val * rhs.val), 
-		(value * (2 * (rhs.dy * rhs.dy) - rhs.val * rhs.dydy)) / (rhs.val * rhs.val * rhs.val));
+	FAD ret(rhs.val / value, 
+			-value * rhs.dx / (rhs.val * rhs.val), 
+			-value * rhs.dy / (rhs.val * rhs.val),
+			(value * (2 * (rhs.dx * rhs.dx) - rhs.val * rhs.dxdx)) / (rhs.val * rhs.val * rhs.val), 
+			(value * (2 * (rhs.dx * rhs.dy) - rhs.val * rhs.dxdy)) / (rhs.val * rhs.val * rhs.val), 
+			(value * (2 * (rhs.dy * rhs.dy) - rhs.val * rhs.dydy)) / (rhs.val * rhs.val * rhs.val));
 	return ret;
 }
 
 // FAD / value
 FAD operator/(const FAD& lhs, double value) {
-	FAD ret(lhs.val / value, lhs.dx / value, lhs.dy / value, lhs.dxdx / value, lhs.dxdy / value, lhs.dydy / value);
+	FAD ret(lhs.val / value, 
+			lhs.dx / value, 
+			lhs.dy / value, 
+			lhs.dxdx / value, 
+			lhs.dxdy / value, 
+			lhs.dydy / value);
 	return ret;
 }
 
-void testCase(int x, int y) {
+FAD sin(const FAD& x) {
+	FAD ret(sin(x.val), 
+			x.dx * cos(x.val), 
+			x.dy * cos(x.val), 
+			x.dxdx * cos(x.val) - (x.dx * x.dx) * sin(x.val), 
+			x.dxdy * cos(x.val) - x.dy * x.dx * sin(x.val), 
+			x.dydy * cos(x.val) - (x.dy * x.dy) * sin(x.val));
+	return ret;
+}
+
+FAD cos(const FAD& x) {
+	FAD ret(cos(x.val), 
+		x.dx * (-sin(x.val)), 
+		x.dy * (-sin(x.val)),
+		(x.dx * x.dx) * (-cos(x.val)) - x.dxdx * sin(x.val),
+		x.dy * x.dx * (-cos(x.val)) - x.dxdy * sin(x.val),
+		(x.dy * x.dy) * (-cos(x.val)) - x.dydy * sin(x.val));
+	return ret;
+}
+
+FAD exp(const FAD& x) {
+	FAD ret(exp(x.val),
+		exp(x.val) * x.dx,
+		exp(x.val) * x.dy,
+		exp(x.val) * ((x.dx * x.dx) + x.dxdx),
+		exp(x.val) * (x.dy * x.dx + x.dxdy),
+		exp(x.val) * ((x.dy * x.dy) + x.dydy)
+		);
+	return ret;
+}
+
+void testCase(double x, double y) {
 	FAD fad_x(x, 1, 0, 0, 0, 0);
 	FAD fad_y(y, 0, 1, 0, 0, 0);
-	cout << funkcja(fad_x, fad_y);
+	cout << std::setprecision(15) << std::showpoint << funkcja(fad_x, fad_y);
 }
 
 int main() {
@@ -134,7 +199,7 @@ int main() {
 	cin >> M;
 	while (M > 0) {
 		M--;
-		int x, y;
+		double x, y;
 		cin >> x >> y;
 		testCase(x, y);
 	}
